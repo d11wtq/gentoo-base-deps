5796c8e9f05efe70387b96a25e3ed1763a42a9f4
d11wtq <chris@w3style.co.uk>
2014-08-03 01:52:20 +0000

    Add some more debug output

--- deps/shared/git_repo.sh
+++ deps/shared/git_repo.sh
@@ -2,11 +2,14 @@ git_repo() {
   src=$1
   dst=$2
 
+  echolog "$src -> $dst"
+
   is_met() {
     [[ -d $dst ]]
   }
 
   meet() {
+    echolog "Cloning $src"
     GIT_SSH=$p/git_repo/git_ssh.sh git clone $src $dst
   }
 }
